684ac896f2599078a555f872072f84d31948dd96
Anthony Chambers <ajfchambers@gmail.com>
2023-03-12 11:18:01 -0400

    spinner2.js created

---
+++ spinner2.js
@@ -0,0 +1,10 @@
+let time = 100;
+const spinnerArray = ['\r|   ', '\r/   ', '\r-   ', '\r\\   '];
+for (let x = 0; x < 50; x++) {
+  for (const spin of spinnerArray) {
+    setTimeout(() => {
+      process.stdout.write(spin);
+    }, time);
+    time += 200
+  };
+};
